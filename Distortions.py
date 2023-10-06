429464c5eaa270af53a66d6e261c749836360f34
tbabineau <tbabine1@unb.ca>
2023-10-05 21:55:43 -0300

    using os.path.isfile instead of except
    Changed from depending on a try except block from trying to create the file to just using os.path.isfile to see if the name of the file corresponds to an existing file

--- Distortions.py
+++ Distortions.py
@@ -1,4 +1,4 @@
-import wave
+import wave, os
 def create_outfile(f_in, f_out):
     f_out.setnchannels(f_in.getnchannels())
     f_out.setsampwidth(f_in.getsampwidth())
@@ -28,10 +28,11 @@ def speedup(file_name, amount):
     if(amount == 0):
         amount = 1
     f_in = f_in = wave.open(file_name + ".wav", "rb")
-    f_out = wave.open(file_name + "_sped_up_x" + str(amount) + ".wav", "wb")
+    f_out = wave.open(file_name + "_sped_up_" + str(amount) + "x.wav", "wb")
 
     f_out = create_outfile(f_in, f_out)
     f_out.setframerate(f_in.getframerate() * amount)
+    #Passing in -1 as an argument to readframes will tell it to read all the frames in
     f_out.writeframes(f_in.readframes(-1))
 
     f_in.close()
@@ -41,7 +42,7 @@ def slow(file_name, amount):
     if(amount == 0):
         amount = 1
     f_in = f_in = wave.open(file_name + ".wav", "rb")
-    f_out = wave.open(file_name + "_slowed_x " + str(amount) + ".wav", "wb")
+    f_out = wave.open(file_name + "_slowed_" + str(amount) + "x.wav", "wb")
 
     f_out = create_outfile(f_in, f_out)
     f_out.setframerate(f_in.getframerate() / amount)
@@ -53,31 +54,34 @@ def slow(file_name, amount):
 
 while(True):
     fname = input("Enter the name of the WAV file you want to edit, excluding the .wav: ")
-
-    try:
-        correct = False
-        while(not correct):
-            print("\nre - reverse the input file")
-            print("sl - slow down the input file")
-            print("sp - speed up the input file")
-            choice = input("").lower()
-
-            if(choice == "re"):
-                reverse(fname)
-                correct = True
-            elif(choice == "sl"):
-                factor = int(input("Enter the factor that the file should be slowed by: "))
-                slow(fname, factor)
-                correct = True
-            elif(choice == "sp"):
-                factor = int(input("Enter the factor that the file should be sped up by: "))
-                speedup(fname, factor)
-                correct = True
-            else:
-                print("That is not a valid option, please pick a valid option")
-    except:
+    while(not os.path.isfile("./" + fname + ".wav")):
         print("Invalid file name.")
+        fname = input("Enter the name of the WAV file you want to edit, excluding the .wav: ")
+    correct = False
+    while(not correct):
+        print("\nre - reverse the input file")
+        print("sl - slow down the input file")
+        print("sp - speed up the input file")
+        choice = input("")
+        choice = choice.lower()
 
-    choice = input("Would you like to edit another file? (y/n): ").lower()
+        if(choice == "re"):
+            reverse(fname)
+            correct = True
+        elif(choice == "sl"):
+            factor = float(input("Enter the factor that the file should be slowed by: "))
+            slow(fname, factor)
+            correct = True
+        elif(choice == "sp"):
+            factor = float(input("Enter the factor that the file should be sped up by: "))
+            speedup(fname, factor)
+            correct = True
+        else:
+            print("That is not a valid option, please pick a valid option")
+    print("\nDone!")
+        
+
+    choice = input("Would you like to edit another file? (y/n): ")
+    choice = choice.lower()
     if(choice == "n"):
         break
